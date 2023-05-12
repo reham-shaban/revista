6dcb347b507ec3e5c4fa989f188efa29711b9859
reham-shaban <reham.shaban2002@gmail.com>
2023-05-12 00:32:16 -0400

    adding main app

--- revista/revista/settings.py
+++ revista/revista/settings.py
@@ -31,12 +31,19 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # Third-party apps
+    'rest_framework',
+    
+    # Local appas
+    'main',
 ]
 
 MIDDLEWARE = [
