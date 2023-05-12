809bfa61f7ee9ff1e738292a8e20d510d2126337
reham-shaban <reham.shaban2002@gmail.com>
2023-05-12 01:43:22 -0400

    setting mysql

--- revista/revista/settings.py
+++ revista/revista/settings.py
@@ -80,11 +80,27 @@ WSGI_APPLICATION = 'revista.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# SQLite
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# MySQL
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': {   
+        'ENGINE': 'django.db.backends.mysql',   
+        'NAME': 'project1',   
+        'USER': 'root',   
+        'PASSWORD': '3028',   
+        'HOST': '127.0.0.1',   
+        'PORT': '3306',   
+        'OPTIONS': {   
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"   
+        }   
+    }   
 }
 
 
